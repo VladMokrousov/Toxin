6856dea920b7107f2d8dad5509eb29fa2ba56432
Vlad-Mokrousov <mokrousov01@ya.ru>
2020-08-09 15:48:35 +0800

    Добавляет влайдер для карточек номеров отеля, правки и улучшения

---
+++ src/forCalendar.js
@@ -0,0 +1,8 @@
+import "./assets/css/normalize.css";
+import "./assets/css/flatpickr.min.css";
+//import "./../node_modules/flatpickr/dist/flatpickr.min.css";
+import "./assets/sass/style.scss";
+
+import "./js/load.js";
+import "./js/slider-for-rooms.js";
+import "./js/calendar.js";
